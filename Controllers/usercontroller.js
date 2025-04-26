57d00a75a6c5069373434126fad48bad80c0e2fb
divyam.20scse1010914@galgotiasuniversity.edu.in <divyam.20scse1010914@galgotiasuniversity.edu.in>
2025-04-26 21:20:08 +0530

    Create and Order Service

--- Controllers/usercontroller.js
+++ Controllers/usercontroller.js
@@ -12,7 +12,7 @@ const signup = async (req, res) => {
             return res.status(400).json({ message: 'User already exists' });
         }
         const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new User({ email, password: hashedPassword, name });
+        const user = new User({ email, password: hashedPassword, name,role });
         await user.save();
 
         res.status(201).json({ message: 'User registered successfully' });
