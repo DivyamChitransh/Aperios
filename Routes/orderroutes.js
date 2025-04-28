6f35e77bb3f728437227fa973e4d19c6bd61650b
divyam.20scse1010914@galgotiasuniversity.edu.in <divyam.20scse1010914@galgotiasuniversity.edu.in>
2025-04-28 12:13:50 +0530

    Checkout and Order Servive

--- Routes/orderroutes.js
+++ Routes/orderroutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { placeOrder, getUserOrders, updateOrderStatus } = require('../Controllers/ordercontroller.js');
 const {authentication} = require('../Controllers/usercontroller.js')
 router.post('/place',authentication, placeOrder);
-router.post('/myorders',authentication, getUserOrders);
+router.get('/myorders',authentication, getUserOrders);
 router.patch('/status/:orderId',authentication, updateOrderStatus);
 
 module.exports = router;
